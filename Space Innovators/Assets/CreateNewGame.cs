f3f981b5c6ab24d63e9e119d134710ae94610a5b
Raya Litova <raya.litova23@gmail.com>
2022-03-11 00:12:19 +0200

    added bedroom and menu

---
+++ Space Innovators/Assets/CreateNewGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNewGame : MonoBehaviour
+{
+    //private GameObject data;
+    void Start()
+    {
+        //data = GameObject.Find("Statics");
+    }
+}
