4380b4f675249a3c414721d9292fa6c91d2335c6
Ad04a <andrey.todorov04@gmail.com>
2022-03-30 11:16:53 +0300

    Fixed all prefabs

--- Space_Innovators/Assets/Scripts/Building/Build.cs
+++ Space_Innovators/Assets/Scripts/Building/Build.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class Build : MonoBehaviour
 {
     [SerializeField] BuildRegulator BL;
-    [SerializeField]GameObject to_build;
+    //[SerializeField]GameObject to_build;
 
     public void build(){
         BL.newX = 6;
         BL.newY = 6;
-        Vector3 objectPOS = Vector3.zero;
+        BL.buildRoom(BL.newX,BL.newY,3);
+        /*Vector3 objectPOS = Vector3.zero;
         objectPOS = new Vector2(-1.3f,-1.2f);
         GameObject newGameObject = Instantiate(to_build, objectPOS, Quaternion.identity);
         BL.map[BL.newX,BL.newY] = 1;
 
         AstarPath.active.Scan();
-        BL.UpdateInteractables();
+        BL.UpdateInteractables();*/
     }
  
 }
--- Space_Innovators/Assets/Scripts/Building/BuildRegulator.cs
+++ Space_Innovators/Assets/Scripts/Building/BuildRegulator.cs
@@ -6,8 +6,9 @@ using Pathfinding;
 public class BuildRegulator : MonoBehaviour
 {
     public int[,] map = new int[11, 11];
-    public int newX;
-    public int newY;
+    public int newX = 5;
+    public int newY = 5;
+    float offset = 7.75f;
     Transform MainRoom;
     [SerializeField] GameObject[] startRooms;
     [SerializeField] GameObject[] Crew;
@@ -31,48 +32,42 @@ public class BuildRegulator : MonoBehaviour
     }
     // bugva zashtoto ne se promenqt tagovete
     // opciq za opravqne e da imam masiv s nachanite i sled tova shte se setvat manualy
-    public void addCrewMate(){
-        Vector3 objectPOS = new Vector3(-3.25f, 8f, 0f);
+    public void addCrewMate(int crewIndex){
+        Vector3 objectPOS = Vector3.zero;
         Debug.Log(GameObject.FindGameObjectsWithTag("Station"));
-        GameObject newGameObject = Instantiate(Crew[0], objectPOS, Quaternion.identity);
+        GameObject newGameObject = Instantiate(Crew[crewIndex], objectPOS, Quaternion.identity);
         newGameObject.GetComponent<NpcAi>().target = GameObject.FindGameObjectsWithTag("Station")[0].transform	;
-         GameObject.FindGameObjectsWithTag("Station")[0].transform.tag = "UsedStation";
+        GameObject.FindGameObjectsWithTag("Station")[0].transform.tag = "UsedStation";
         newGameObject.transform.GetComponent<NpcAi>().bed = GameObject.FindGameObjectsWithTag("Bed")[0].transform;
         GameObject.FindGameObjectsWithTag("Bed")[0].transform.tag = "UsedBed";
         newGameObject.transform.GetComponent<NPCStats>().camera = GameObject.Find("Main Camera").transform;
     }
 
+    public void buildRoom(int newX, int newY, int roomIndex){
+        Vector3 objectPOS = Vector3.zero;
+        objectPOS = new Vector3((float)(objectPOS.x+((newX-5f)*offset)), (float)(objectPOS.y+((newY-5f)*offset)), 0f);
+        GameObject newGameObject = Instantiate(startRooms[roomIndex], objectPOS, Quaternion.identity);
+        map[newX,newY] = 1;
+        AstarPath.active.Scan();
+    }
+
    void Start()
     {
-        //GameObject testPrefab = new Comms();
-        Vector3 objectPOS = Vector3.zero;
-        GameObject newGameObject = Instantiate(startRooms[0], objectPOS, Quaternion.identity);
-        newGameObject.SetActive(true);
-        MainRoom = newGameObject.transform;
-        map[5,5] = 1;
-        //map[6,5] = 1;
+        buildRoom(newX,newY,0);
+
         newX = 4;
         newY = 5;
-        objectPOS = Vector3.zero;
-        objectPOS = new Vector3((float)(objectPOS.x+((newX-5)*8)), (float)(objectPOS.y+((newY-5)*8.15)), 0f);
-        newGameObject = Instantiate(startRooms[1], objectPOS, Quaternion.identity);
-        map[newX,newY] = 1;
+        buildRoom(newX,newY,1);
+
         newX = 5;
         newY = 6;
-        objectPOS = Vector3.zero;
-        objectPOS = new Vector3((float)(objectPOS.x+((newX-5)*8)), (float)(objectPOS.y+((newY-5)*8.15)), 0f);
-        newGameObject = Instantiate(startRooms[2], objectPOS, Quaternion.identity);
-        map[newX,newY] = 1;
+        buildRoom(newX,newY,2);
 
-        AstarPath.active.Scan();
-        //UpdateInteractables();
         Debug.Log(stations);
-        
-        //UpdateInteractables();
-        //Debug.Log(stations);
-        addCrewMate();
-        addCrewMate();
-        //UpdateInteractables();
+
+       // addCrewMate(0);
+        addCrewMate(0);
+     
     }
 
 }
