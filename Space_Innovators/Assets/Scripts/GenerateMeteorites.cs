e36ff049e0020d5546f5735ef81e75f707389d0d
Raya Litova <raya.litova23@gmail.com>
2022-04-05 01:05:48 +0300

    Added lab functions

--- Space_Innovators/Assets/Scripts/GenerateMeteorites.cs
+++ Space_Innovators/Assets/Scripts/GenerateMeteorites.cs
@@ -11,12 +11,14 @@ public class GenerateMeteorites : MonoBehaviour
     private System.Random random = new System.Random();
 
     void Update(){
+        if(meteorite.transform.position.x > 100 || meteorite.transform.position.y > 100)
+            meteorite.transform.position = new Vector2(random.Next(-100, 100),random.Next(-100, 100));
+
         if (Time.time > nextActionTime){
             nextActionTime += period * (1+random.Next(5));
             if(!meteorite.activeSelf){
                 meteorite.SetActive(true);
                 meteorite.transform.position = new Vector2(random.Next(-100, 100),random.Next(-100, 100));
-
             }
             
         }
